a59131b5ed9be7654f5778ffa656d324e1c9398d
Gabriel <isporck0@gmail.com>
2019-11-27 15:39:39 -0200

    .

--- p2.py
+++ p2.py
@@ -1,16 +1,18 @@
 
-def troca(z):
-    lita = []
-    for i in z:
-        if i.isupper() == True:
-            lita.append(i.lower())
-        elif i.islower() == True:
-            lita.append(i.upper())  
-        else:
-            lita.append('')    
-    print(lita)
-
+f = open('p2.txt',"w")
 fr1 = ( input('algo:'))
-x = troca(fr1)
-
-
+fr2 = ( input('dnv:'))
+f.write(fr1+"\n")
+f.write(fr2)
+f.close()
+x = open('p2.txt','r')
+zet = open('p2f.txt',"w")
+for i in x:
+    if i.isupper() == True:
+        zet.write(i.lower())
+    elif i.islower() == True:
+        zet.write(i.upper())
+    elif i.isspace() == True:
+        pass
+x.close()
+zet.close()
