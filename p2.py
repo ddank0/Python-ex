524799dc6c234d2bdc850af9104a45f9a863b57a
Gabriel <isporck0@gmail.com>
2019-11-27 14:36:33 -0200

    ok

--- p2.py
+++ p2.py
@@ -1,16 +1,16 @@
+
 def troca(z):
-
-        if z.isupper() == True:
-            z.lower()
-
-        elif z.islower() == True:
-            z.upper()
-        return z
+    lita = []
+    for i in z:
+        if i.isupper() == True:
+            lita.append(i.lower())
+        elif i.islower() == True:
+            lita.append(i.upper())  
+        else:
+            lita.append('')    
+    print(lita)
 
 fr1 = ( input('algo:'))
-
 x = troca(fr1)
-
-print(x)
 
 
