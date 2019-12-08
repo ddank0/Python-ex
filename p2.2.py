750a6101a81ea9d87abd3845b6a3a35d32aba2a2
Gabriel <isporck0@gmail.com>
2019-12-08 20:23:56 -0200

    +

---
+++ p2.2.py
@@ -0,0 +1,32 @@
+def matrix(x):
+    aux = []
+    for j in range(len(x[0])):
+        linha = []
+        for i in range(len(x)):
+            linha.append(x[i][j])
+        aux.append(linha)
+    return aux
+
+mt = []
+tax = int(input('x:'))
+tay = int(input('y:'))
+for i in range(0,tax):
+    mt.append(0)
+    mt[i] = []
+    for j in range(0,tay):
+        mt[i].append(int(input('elem:')))
+
+for i in range(0,tax):
+    for j in range(0,tay):
+        print(f'[{mt[i][j]:^5}]',end='')
+    print()
+
+
+y = matrix(mt)
+
+print('')
+
+for i in range(0,tax):
+    for j in range(0,tay):
+        print(f'[{y[i][j]:^5}]',end='')
+    print()
