52c41d09f0b4043e9b984e816fc5c36b8fad5fde
Gabriel <isporck0@gmail.com>
2019-12-09 03:12:24 -0200

    é isso

---
+++ S4 tabuada.py
@@ -0,0 +1,13 @@
+for md in range(1,11):
+    print(f'''{'....'*10}
+            .    {md} x  1 = {md}     
+            .    {md} x  2 = {md*2}   
+            .    {md} x  3 = {md*3}   
+            .    {md} x  4 = {md*4}   
+            .    {md} x  5 = {md*5}   
+            .    {md} x  6 = {md*6}   
+            .    {md} x  7 = {md*7}   
+            .    {md} x  8 = {md*8}   
+            .    {md} x  9 = {md*9}   
+            .    {md} x 10 = {md*10}  
+{'....'*10}''')
